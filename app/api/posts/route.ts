200c4bc0e25e0def16cbe9d312b817b7772f1c90
MountainGurl <palomita@Palomas-MacBook-Air.local>
2023-10-16 20:20:03 -0400

    connect db using prisma

---
+++ app/api/posts/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export async function GET(req: NextRequest) {
+ 
+  const posts = await prisma.posts.findMany()
+
+  return NextResponse.json(posts)
+}
